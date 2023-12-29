07e5191cc1272e411fa12b2b8df417c95c70be9d
KSH <ingnam920@gmail.com>
2023-12-29 12:37:09 +0900

    커밋2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
     // to see how IntelliJ IDEA suggests fixing it.
     System.out.printf("새프로젝트 커밋");
     System.out.print("새프로젝트 커밋2");
+    System.out.println("ㄹ자둘ㅈㄷtestg");
 
 
   }
