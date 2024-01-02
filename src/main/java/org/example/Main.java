06d0e3477b31fcad7c6498186f58e2e0fd869616
KSH <ingnam920@gmail.com>
2024-01-02 11:48:59 +0900

    등록,수정,삭제,조회 index생각

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,19 +23,23 @@ public class Main {
       if (cmd.equals("exit")) {
         break;
       }
-      if (cmd.equals("write")) {
+      if (cmd.startsWith("article write")) {
         String date = Util.getNowDate_TimeStr();
+        String update = Util.getNowDate_TimeStr();
         String s1 = sc.nextLine();
         String s2 = sc.nextLine();
-        articlearr.add(new Article(id, date, s1, s2));
+        articlearr.add(new Article(id, date, update, s1, s2));
         id++;
 
 
       }
-      if (cmd.equals("list")) {
+      if (cmd.startsWith("article list")) {
         Iterator<Article> e = articlearr.listIterator();
         while (e.hasNext()) {
           System.out.println(e.next().toString());
+        }
+        if (articlearr == null || articlearr.isEmpty()) {
+          System.out.println("게시글이없습니다.");
         }
       }
       if (cmd.startsWith("article detail")) {
@@ -43,17 +47,42 @@ public class Main {
         boolean has = false;
         int a = Integer.parseInt(str[2]);
         Iterator<Article> e = articlearr.listIterator();
+        int found = 0;
         while (e.hasNext()) {
           if (e.next().getNum() == a) {
+
             has = true;
             break;
           }
+          found++;
         }
         if (!has) {
           System.out.println("님이 찾는게 세부목록에없습니다.");
           continue;
         }
-        System.out.println(articlearr.get(a - 1));
+        System.out.println(articlearr.get(found));
+      }
+      if (cmd.startsWith("article delete")) {
+        String[] str = cmd.split(" ");
+        int a = Integer.parseInt(str[2]);
+        Iterator<Article> e = articlearr.listIterator();
+        int found = 0;
+        boolean has = false;
+        while (e.hasNext()) {
+          if (e.next().getNum() == a) {
+            has = true;
+            Article art = e.next();
+            articlearr.remove(found);
+            System.out.println(a + "번째 게시글이 삭제되었습니다.");
+            break;
+          }
+          found++;
+        }
+        if (!has) {
+          System.out.println(a + "번째 게시글이 없습니다");
+        }
+
+
       }
     }
     System.out.println("==프로그램 끝==");
@@ -66,12 +95,14 @@ class Article {
   private String name;
   private String naeyong;
   private String date;
+  private String update;
 
-  public Article(int num, String date, String name, String naeyong) {
+  public Article(int num, String date, String update, String name, String naeyong) {
     this.num = num;
     this.name = name;
     this.naeyong = naeyong;
     this.date = date;
+    this.update = update;
   }
 
   @Override
@@ -81,6 +112,7 @@ class Article {
         ", name='" + name + '\'' +
         ", naeyong='" + naeyong + '\'' +
         ", date='" + date + '\'' +
+        ", update='" + update + '\'' +
         '}';
   }
 
